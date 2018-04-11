a7785ed914ac7240af6fe89c007e4fea855028da
prsen <sen.przemyslaw@gmail.com>
2018-04-11 18:50:28 +0200

    Initial commit; gitignore

---
+++ src/main/java/com/blinenterprise/SyropKlonowy/SyropKlonowyApplication.java
@@ -0,0 +1,12 @@
+package com.blinenterprise.SyropKlonowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyropKlonowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyropKlonowyApplication.class, args);
+	}
+}
