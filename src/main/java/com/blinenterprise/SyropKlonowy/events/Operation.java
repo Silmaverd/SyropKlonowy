a2edbe024d17bcb50fa0ad25ac6fea1e2f507c34
prsen <sen.przemyslaw@gmail.com>
2018-04-18 18:16:52 +0200

    #19: CR fix

--- src/main/java/com/blinenterprise/SyropKlonowy/events/Operation.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/events/Operation.java
@@ -1,14 +1,16 @@
 package com.blinenterprise.SyropKlonowy.events;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.UUID;
 
 @Slf4j
+@Getter
 public abstract class Operation {
-    public String username;
-    public UUID processID;
-    public String description;
+    protected String username;
+    protected UUID processID;
+    protected String description;
 
     public void persist(){
         log.info("Operation " + description + " invoked by " + username + " within process " + processID.toString());
