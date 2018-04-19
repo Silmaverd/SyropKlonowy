efb75f9f89bcb07c1dee904e664585305992d29c
Konrad Szmit <konrad.t.szmit@gmail.com>
2018-04-19 19:05:18 +0200

    #17 Useless change to enable pull request

--- src/main/java/com/blinenterprise/SyropKlonowy/service/ProductService.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/service/ProductService.java
@@ -11,6 +11,8 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+
+
 @Service
 public class ProductService {
     private ProductRepository productRepository;
