22bca1b6a5f5b29c8ce6761dd25097c2b2dca43f
Justyna Pawluczuk <justyna950803@gmail.com>
2018-04-21 15:19:02 +0200

    #24. ProductRepository has been expanded with a custom query. Added test for ProductRepository.

--- src/main/java/com/blinenterprise/SyropKlonowy/repository/ProductRepository.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/repository/ProductRepository.java
@@ -1,10 +1,26 @@
 package com.blinenterprise.SyropKlonowy.repository;
 
+import com.blinenterprise.SyropKlonowy.domain.Category;
 import com.blinenterprise.SyropKlonowy.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
+    Product findByName(String name);
+
+    Product findById(long id);
+
+    List<Product> findAllByCategory(Category category);
+
+    List<Product> findAllByPriceLessThanEqual(Double price);
+
+    List<Product> findAllByPriceGreaterThanEqual(Double price);
+
+    List<Product> findAllByPriceLessThanEqualAndCategory(Double price, Category category);
+
+    List<Product> findAllByPriceGreaterThanEqualAndCategory(Double price, Category category);
 }
