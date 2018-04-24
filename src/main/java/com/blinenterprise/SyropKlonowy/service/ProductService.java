f9f3e76fb246602786b3ae4dac43f15d888f3a8f
Konrad Szmit <konrad.t.szmit@gmail.com>
2018-04-24 13:38:18 +0200

    #17: Added CrudService and ProductService

--- src/main/java/com/blinenterprise/SyropKlonowy/service/ProductService.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/service/ProductService.java
@@ -2,50 +2,11 @@ package com.blinenterprise.SyropKlonowy.service;
 
 import com.blinenterprise.SyropKlonowy.domain.Product;
 import com.blinenterprise.SyropKlonowy.repository.ProductRepository;
-import com.sun.xml.internal.bind.v2.model.core.ID;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
 
 
 @Service
-public class ProductService {
-    private ProductRepository productRepository;
-
-    @Autowired
-    public void setProductRepository(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
-    public boolean exists(Long id){
-        return this.productRepository.existsById(id);
-    }
-
-    public List<Product> findAll(){
-        return StreamSupport.stream( productRepository.findAll().spliterator(), false).collect(Collectors.toList());
-    }
-
-    public Optional<Product> findById(Long id) {
-        return this.productRepository.findById(id);
-    }
-
-    public void save(Product product) {
-        this.productRepository.save(product);
-    }
-
-    public Long count(){
-        return this.productRepository.count();
-    }
-
-    public void delete(Product product) {
-        this.productRepository.delete(product);
-    }
-
-
+public class ProductService extends CrudService<Product, Long, ProductRepository>{
 
 }
