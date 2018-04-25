d8513b95e625281fb6627c17f1584ec4bd67aa98
Konrad Szmit <konrad.t.szmit@gmail.com>
2018-04-25 20:11:36 +0200

    #29: Added ListResponse error constructor.

--- src/main/java/com/blinenterprise/SyropKlonowy/web/ListResponse.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/web/ListResponse.java
@@ -1,10 +1,14 @@
 package com.blinenterprise.SyropKlonowy.web;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ListResponse<T extends View> extends Response<T>{
     private List<T> payload;
 
+    public ListResponse(Boolean ok, Optional<String> errorMessage) {
+        super(ok, errorMessage);
+    }
 
     public ListResponse(Boolean ok, List<T> payload) {
         this.ok = ok;
