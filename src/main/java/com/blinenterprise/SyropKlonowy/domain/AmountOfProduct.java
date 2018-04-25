dc81b5b441cb8caf56d85fe57dee2a0aa74af175
Shirke <asmax6@gmail.com>
2018-04-25 19:47:55 +0200

    #36: remove @ToString

--- src/main/java/com/blinenterprise/SyropKlonowy/domain/AmountOfProduct.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/domain/AmountOfProduct.java
@@ -5,7 +5,6 @@ import lombok.*;
 import javax.persistence.*;
 
 @Getter
-@ToString
 @NoArgsConstructor
 @Entity
 public class AmountOfProduct {
--- src/main/java/com/blinenterprise/SyropKlonowy/domain/Warehouse.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/domain/Warehouse.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Getter
-@ToString
 @NoArgsConstructor
 @Entity
 public class Warehouse {
