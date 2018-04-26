2dfc663b17da27da00843498e5fd13cf78b0fe14
Justyna Pawluczuk <justyna950803@gmail.com>
2018-04-26 23:22:33 +0200

    #30: Next corrections

--- src/main/java/com/blinenterprise/SyropKlonowy/api/ClientApi.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/api/ClientApi.java
@@ -4,6 +4,7 @@ package com.blinenterprise.SyropKlonowy.api;
 import com.blinenterprise.SyropKlonowy.domain.Address;
 import com.blinenterprise.SyropKlonowy.domain.Client;
 import com.blinenterprise.SyropKlonowy.domain.Enterprise;
+import com.blinenterprise.SyropKlonowy.service.AddressService;
 import com.blinenterprise.SyropKlonowy.service.ClientService;
 import com.blinenterprise.SyropKlonowy.web.ClientView;
 import com.blinenterprise.SyropKlonowy.web.Response;
@@ -21,18 +22,24 @@ public class ClientApi {
     @Autowired
     ClientService clientService;
 
-    @RequestMapping(path = "/client/add", method = {RequestMethod.PUT})
+    @Autowired
+    AddressService addressService;
+
+    @RequestMapping(path = "/client/addClient", method = {RequestMethod.PUT})
     @ApiOperation(value = "Add a client", response = Response.class)
-    public Response<ClientView> AddClient(@RequestParam(value = "name", required = true)String name,@RequestParam(value = "company", required = true) String company,
+    public Response<ClientView> addClient(@RequestParam(value = "name", required = true)String name,@RequestParam(value = "company", required = true) String company,
                                           @RequestParam(value = "street", required = true) String street, @RequestParam(value = "buildingNumber", required = true) String buildingNumber,
                                           @RequestParam(value = "city", required = true) String city, @RequestParam(value = "zipCode", required = true) int zipCode,
                                           @RequestParam(value = "enterpriseType", required = true) Enterprise enterpriseType) {
-        Client client = new Client(name, company, false, new Address(street, buildingNumber, city, zipCode), enterpriseType);
-        if(clientService.create(client).equals(client)) {
+        try{
+            Address address = new Address(street, buildingNumber, city, zipCode);
+            addressService.create(address);
+            Client client = new Client(name, company,false, address, enterpriseType);
+            clientService.create(client);
             return new Response<ClientView>(true, Optional.empty());
         }
-        else{
-            return new Response<ClientView>(true, Optional.of("failure to create client"));
+        catch (Exception e){
+            return new Response<ClientView>(false, Optional.of(e.getMessage()));
         }
     }
 
--- src/main/java/com/blinenterprise/SyropKlonowy/service/ClientService.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/service/ClientService.java
@@ -16,7 +16,7 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-    public Client create(Client client){return clientRepository.save(client);}
+    public void create(Client client){clientRepository.save(client);}
 
     public Client findById(long id){
         Optional<Client> mayBeClient= clientRepository.findById(id);
