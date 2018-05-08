dedb6c6fccc08e5845bf1f444c783160f61c88fe
Shirke <asmax6@gmail.com>
2018-05-08 23:13:30 +0200

    #44: added findByName

--- src/main/java/com/blinenterprise/SyropKlonowy/repository/ClientRepository.java
+++ src/main/java/com/blinenterprise/SyropKlonowy/repository/ClientRepository.java
@@ -1,13 +1,17 @@
 package com.blinenterprise.SyropKlonowy.repository;
 
 import com.blinenterprise.SyropKlonowy.domain.Client;
+import com.blinenterprise.SyropKlonowy.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends CrudRepository<Client, Long> {
 
     Iterable<Client> findAllByName(String name);
 
+    Optional<Client> findByName(String name);
 }
 
